7db1021e31d42f4115438e8c6a3eeaa737f36791
Weidom12 <72113218+Weidom12@users.noreply.github.com>
2022-11-08 23:08:21 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>
+   
+    <Navbar/>
+    <div className="App">
+    <header className="App-header">
+    <h1 className="title">Bienvenido a MatchPet</h1> 
+    <div id='apartado1'> 
+    <p> MatchPet surgio como una idea para contribuir con la problematica
+        que hoy en dia se volvio tan comun que es el avandono de animales
+        con esta app web podras poner en adopcion mascotas que buscan familia 
+        o podras tu adoptar mascotas que necesitan amor y felicidad.        
+      </p>    
+      
+      <p>
+        Ademas nuestra app web tiene una funcion que te permitira poner alertas
+        de que tu mascota esta perdida y asi todos los que estan cerca de ti 
+        podran ayudarte a encontrarla.
+      </p>
+
+      <p>
+         Finalmente contamos con un chat el cual puedes utilizar para contactarte
+         con la persona que encontro tu madcota (en el caso de que se extraviara)
+         o tambien contactarse con la persona que quiere adoptar la mascota que 
+         pusiste en adopcion o viceversa.
+      </p>
+      </div>  
+    </header>
+    
+    </div>
+    </>
+   
+  );
+}
+
+export default App;
